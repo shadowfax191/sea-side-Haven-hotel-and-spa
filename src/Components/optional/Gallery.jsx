e8bdee372a55259fea7a416f4c91a40c5a843424
git config --global user.name shadowfax191 <shahariarhossain0599@gmail.com>
2023-11-08 12:59:12 +0600

    showing toast for crud op

--- src/Components/optional/Gallery.jsx
+++ src/Components/optional/Gallery.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Helmet } from "react-helmet-async";
+import toast from "react-hot-toast";
 
 
 const Gallery = () => {
     window.scrollTo(0,0)
-
+const handleContact=(e)=>{
+    e.preventDefault()
+    toast.success('Massage Send')
+    e.target.reset()
+}
     return (
         <div>
             <Helmet>
@@ -34,11 +39,15 @@ const Gallery = () => {
                    
                 </div>
                 <div className="text-center  space-y-3 pt-10">
-                        <p className="text-3xl font-bold">Contact Form</p>
-                        <p className="font-semibold text-lg">Name: <input type="text" placeholder="Type here" className="input input-bordered input-accent w-full max-w-xs" /> </p>
-                        <p className="font-semibold text-lg">Contact: <input type="text" placeholder="Type here" className="input input-bordered input-accent w-full max-w-xs" /></p>
-                        <p className="font-semibold text-lg">Email: <input type="text" placeholder="Type here" className="input input-bordered input-accent w-full max-w-xs" /></p>
-                        <button className="btn btn-accent">submit</button>
+                       <form onSubmit={handleContact} className=" space-y-3 pt-10">
+                       <p className="text-3xl font-bold">Contact Form</p>
+                        <p className="font-semibold text-lg ">Name: <input type="text" required  placeholder="Name" className="input  input-bordered input-accent w-full max-w-xs" /> </p>
+                        <p className="font-semibold text-lg">Contact: <input type="number" required placeholder="Contact" className="input input-bordered input-accent w-full max-w-xs" /></p>
+                        <p className="font-semibold text-lg">Email:  <input type="text" required placeholder="Email" className="input input-bordered input-accent w-full max-w-xs" /></p>
+                        <p className="font-semibold text-lg">Message: <input type="text" required placeholder="Type your message here" className="input input-bordered input-accent w-full max-w-xl" /></p>
+                        <input  className="btn btn-accent" type="submit" />
+                     
+                       </form>
                     </div>
             </div>
         </div>
